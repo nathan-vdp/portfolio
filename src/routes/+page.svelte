bbf255be70d28b0a0eebb0c3441d0ef0e4878bce
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2024-06-19 15:24:34 +0200

    homepage content gecentreerd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <main>
     <div style="position: relative; width: 100%; height: 100vh;">
         <img src={Background} alt="background" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 0; object-fit: cover;" />
-        <div style="position: relative; z-index: 1;">
+        <div style="position: relative; z-index: 1;" class="flex items-center justify-center">
             <Homepage />
         </div>
     </div>
