8d8552d4ac9ba9639591eff6292165353817da57
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2024-06-25 12:37:51 +0200

    .

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,24 +1,17 @@
 <script>
-
+    import { div } from 'three/examples/jsm/nodes/Nodes.js';
+    import Background from '../lib/assests/about-img.62b47e7f183d4b4e9feb.webp';
 </script>
 
-<style>
-    .custom-bg {
-        background: linear-gradient(145deg, #6d28d9, #a855f7);
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    }
-</style>
-
-<main id="projects" class="bg-slate-800 bg-cover text-white">
-    <div class="flex flex-col md:flex-row md:justify-end items-center text-2xl font-bold w-full xl:pr-64">
-        <h1 class="rounded-full custom-bg py-2 px-12 mt-36">About Me</h1>
-    </div>
-    <div class="flex justify-end xl:pr-56">
-        <p class="w-60">I'm a front-end developer
-            that thrives in making sleek websites
-            Proficient with HTML, CSS and JS. I also know my way around Svelte,
-            Tailwind and Bootstrap. In my free time I mostly spend my time on
-            playing bass and going to concerts.
-        </p>
+<main id="projects" class="flex justify-center items-center mt-20">
+    <div class="flex justify-center items-center">
+        <div class="flex flex-col sm:flex-row items-center text-center sm:text-left">
+            <img src={Background} alt="background" class="rounded-lg w-72 mb-4 sm:mb-0 sm:mr-5" />
+            <div>
+                <h1 class="text-blue-600 text-2xl font-bold">About Me</h1>
+                <p class="font-bold text-2xl">Front-End Developer Based in The Netherlands 📍</p>
+                <p class="text-slate-700">Hey, my name is Nathan, and I'm a Frontend Developer. My passion is to create and develop a clean UI/UX for my users.</p>
+            </div>
+        </div>
     </div>
 </main>
