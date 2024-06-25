3f7b597720cd09d3623386a0271665fc7f3be50f
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2024-06-25 11:16:54 +0200

    About me responsief gemaakt button

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -10,7 +10,15 @@
 </style>
 
 <main id="projects" class="bg-slate-800 bg-cover text-white">
-    <div class="flex flex-col md:flex-row justify-center md:justify-end items-center text-2xl font-bold w-full">
-        <h1 class="rounded-full custom-bg py-2 px-12 mt-5 md:mt-5">Projects</h1>
+    <div class="flex flex-col md:flex-row md:justify-end items-center text-2xl font-bold w-full xl:pr-64">
+        <h1 class="rounded-full custom-bg py-2 px-12 mt-36">About Me</h1>
+    </div>
+    <div class="flex justify-end xl:pr-56">
+        <p class="w-60">I'm a front-end developer
+            that thrives in making sleek websites
+            Proficient with HTML, CSS and JS. I also know my way around Svelte,
+            Tailwind and Bootstrap. In my free time I mostly spend my time on
+            playing bass and going to concerts.
+        </p>
     </div>
 </main>
