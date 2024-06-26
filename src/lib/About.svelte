42a0531c7870f2c070b4a5467cdb1fe019966e82
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2024-06-26 10:25:31 +0200

    Alles staat nu netjes in het midden

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -4,12 +4,12 @@
 
 <main id="projects" class="flex justify-center items-center mt-20">
     <div class="flex justify-center items-center">
-        <div class="flex flex-col sm:flex-row items-center text-center sm:text-left">
+        <div class="flex flex-col sm:flex-row items-center text-center sm:text-left xl:ml-48">
             <img src={Background} alt="background" class="rounded-lg w-72 mb-4 sm:mb-0 sm:mr-5" />
-            <div>
+            <div class="px-5">
                 <h1 class="text-blue-600 text-2xl font-bold">About Me</h1>
                 <p class="font-bold text-2xl">Front-End Developer Based in The Netherlands 📍</p>
-                <p class="text-slate-700">Hey, my name is Nathan, and I'm a Frontend Developer. My passion is to create and develop a clean UI/UX for my users.</p>
+                <p class="text-slate-700">Hey, my name is Nathan, and I'm a Frontend Developer. <br> My passion is to create and develop a clean UI/UX for my users.</p>
             </div>
         </div>
     </div>
